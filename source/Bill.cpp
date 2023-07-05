54263db86235010c345c5da5779f26b93ffcbf69
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2023-07-05 08:10:40 +0330

    Create Bill.cpp

---
+++ source/Bill.cpp
@@ -0,0 +1,18 @@
+#include "Bill.hpp"
+
+Bill::Bill(int cost_, int paid_, std::string description_)
+{
+    dept = cost_;
+    paid = paid_;
+    description = description_;
+}
+
+void Bill::setDebt(int precost)
+{
+    dept = dept - precost;
+}
+
+void Bill::setPaid(int precost)
+{
+    paid = paid + precost;
+}
